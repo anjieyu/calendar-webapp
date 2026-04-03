d819eaac8ca5d84b6af4d7b9aae0e92c857e8485
An-Jie Yu <anjieyu9@gmail.com>
2026-04-02 17:36:29 -0700

    finish routes for events api and start db

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,26 @@ class Event(BaseModel):
     date: str
     description: str = ""
 
+def connect_db():
+    connection = sqlite3.connect("events.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def start_db():
+    connection = connect_db()
+    connection.execute("""
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            date TEXT NOT NULL,
+            description TEXT DEFAULT ''    
+        )
+    """)
+    connection.commit()
+    connection.close()
+
+start_db()
+
 @app.get("/")
 def root():
     return {"message": "ok"}
@@ -27,3 +47,11 @@ def get_all_events():
 @app.post("/events")
 def create_event(event: Event):
     return {"message": "add the event to the database"}
+
+@app.get("/events/{date}")
+def get_events_by_date(date: str):
+    return {"message": "get all events on a specific day"}
+
+@app.delete("/events/{event_id}")
+def delete_event(event_id: int):
+    return {"message": "delete an event by a specific id"}
